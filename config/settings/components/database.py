5899ec4f298ea69a1aef2b96dc6e1f4a85fdd6f8
권재원 <springkjw@gmail.com>
2019-07-13 16:16:47 +0900

    데이터베이스 추가 및 postgresql 관련 패키지 추가

--- config/settings/components/database.py
+++ config/settings/components/database.py
@@ -1,6 +1,10 @@
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environn.get('DATABASE_DB'),
+        'USER': os.environ.get('DATABASE_USER'),
+        'PASSWORD': os.environ.get('DATABASE_PASSWORD'),
+        'HOST': os.environ.get('DATABASE_HOST'),
+        'PORT': '5432',
     }
 }
