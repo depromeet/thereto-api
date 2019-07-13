75e5495b8a4ba3fc55266c9a5aeb5e01475057ac
권재원 <springkjw@gmail.com>
2019-07-13 16:43:18 +0900

    api 구조 생성

--- api/urls.py
+++ api/urls.py
@@ -1,2 +1,7 @@
-from django.urls import path
+from django.urls import path, include
 
+urlpatterns = [
+    path('user/', include('api.user.urls', 'user'), namespace='user'),
+    path('friend/', include('api.friend.urls', 'friend'), namespace='friend'),
+    path('letter/', include('api.letter.urls', 'letter'), namespace='letter'),
+]
